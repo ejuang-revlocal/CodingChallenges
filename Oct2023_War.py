aab659b4fb242e264a439f7c4d2200062bff83cb
Eric Juang <ejuang@revlocal.com>
2023-09-28 16:58:18 -0400

    rename, round counter

--- Oct2023_War.py
+++ Oct2023_War.py
@@ -3,8 +3,8 @@ import random
 deck = []
 player1Hand = []
 player2Hand = []
+totalRounds = 0
 gameAutoRun = False
-
 suits = ["\u2660", "\u2663", "\u2665", "\u2666"]
 cardNumbers = [("2",2), ("3",3), ("4",4), ("5",5), ("6",6), ("7",7), ("8",8), ("9",9), ("10",10), ("J",11), ("Q", 12), ("K", 13), ("A", 14)]
 
@@ -30,9 +30,9 @@ def displayScore():
 
 def winCondition():
     if(len(player1Hand) > 0):
-        print("Player 1 Wins!")
+        print(f"Player 1 Won In {totalRounds} Rounds!")
     else:
-        print("Player 2 Wins!")
+        print(f"Player 2 Won In {totalRounds} Rounds!")
     quit()
 
 #Generate Starting Hands
@@ -56,20 +56,18 @@ while len(deck) > 0:
     player2Hand.append(deck.pop())
 
 #Game Setup
-print("Enter 'y' to let the game run itself to the end")
+print("Enter 'y' to let the game run itself to the end. Otherwise, enter.")
 userInput = input()
 if userInput == "y":
     gameAutoRun = True
 
 #Play War
 while len(player1Hand) > 0 and len(player2Hand) > 0:
-
     if not gameAutoRun:
         input("Press Enter to Draw Cards")
     addPlayerCardsToDeck(1)
     player1Card = deck[len(deck) - 2]
     player2Card = deck[len(deck) - 1]
-
     displayDeck()
 
     if player1Card.value == player2Card.value:
@@ -86,15 +84,15 @@ while len(player1Hand) > 0 and len(player2Hand) > 0:
             winCondition()
         addPlayerCardsToDeck(3)
         displayDeck()
-    elif player1Card.value > player2Card.value:
-        print("Player 1 Won This Hand")
-        player1Hand.extend(deck)
-        displayScore();
-        deck.clear()
     else:
-        print("Player 2 Won This Hand")
-        player2Hand.extend(deck)
+        random.shuffle(deck)
+        if player1Card.value > player2Card.value:
+            print("Player 1 Won This Hand")
+            player1Hand.extend(deck)
+        else:
+            print("Player 2 Won This Hand")
+            player2Hand.extend(deck)
         displayScore()
         deck.clear()
-    
+    totalRounds += 1
 winCondition()
